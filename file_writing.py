f79bd893e53c4562d93fc93094464c78cccc99a4
shamikhan14 <147740111+shamikhan14@users.noreply.github.com>
2026-06-22 23:31:38 +0530

    Added append mode file writing example

--- file_writing.py
+++ file_writing.py
@@ -1,3 +1,2 @@
-with open("output.txt", "w") as file:
-    file.write("This is my first program to write a file in python.\n")
-    file.write("Python file writing is working.\n")
+with open("output.txt", "a") as file:
+    file.write("This will append a line\n")
